e0aee70d52bc99dc9ab52ef45ea6339f80fdae62
loizvla <loizvla@gmail.com>
2022-08-22 13:33:10 +0300

    Add files via upload

---
+++ footer_signup.php
@@ -0,0 +1,49 @@
+<div class="container">
+	<!-- Footer and signup form -->
+	<hr>
+		<div class="row py-4 text-muted">
+			<div class="col-md col-xl-5">
+				<p><strong>About E-Care</strong></p>
+				<p>Μέσα σε ένα διαρκώς μεταβαλλόμενο κοινωνικό και οικονομικό περιβάλλον, ο Όμιλος συνεχίζει την αναπτυξιακή του πορεία υλοποιώντας ένα πολυδιάστατο επενδυτικό πρόγραμμα με στόχο την επέκταση σε ευρωπαϊκές χώρες. </p>
+			</div>
+			<div class="col-md col-xl-5 ml-auto">
+				<p><strong>News Letter</strong></p>
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Email">
+					<span class="input-group-btn">
+						<button class="btn btn-primary" type="button">Sign up</button>
+					</span>
+				</div>
+			</div>
+		</div>
+		<hr><!-- /signup form -->
+
+		<!-- footer -->
+		<div class="row py-3">
+			<div class="col-md-7">
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link" href="#">Who we are</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Privacy policy</a>
+					</li>
+					<li class="nav-item">
+					<a class="nav-link" href="#">Commitee Cultire</a>
+					</li>
+					<li class="nav-item">
+					<a class="nav-link" href="#">Our constitutions</a>
+					</li>
+					<li class="nav-item">
+					<a class="nav-link" href="#">Payment methods</a>
+					</li>
+					<li class="nav-item">
+					<a class="nav-link" href="#">Send methods</a>
+					</li>
+				</ul>
+			</div>
+			<div class="col-md text-md-right">
+				<small>&copy; 2020 Full Stack Dev &amp; E-Care</small>
+			</div>
+		</div><!-- /footer --><!-- /signup form -->
+</div>
