a0a58d5261e3482cbab67734e189f0d6584a77e0
loizvla <loizvla@gmail.com>
2022-08-22 13:32:32 +0300

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,10 @@
+<!-- banner -->
+<div class="banner banner-fluid bg-info text-white">
+	<div class="container text-sm-center pt-5">
+		<h1 class="display-2">E-Care</h1>
+		<p class="lead">
+		Καλώς ήλθατε στον κόσμο της Υγείας!
+		</p>
+	</div>
+</div>
+<!-- /banner -->
