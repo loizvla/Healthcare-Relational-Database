4360d2c5ac6bd6c8922efc702ed51ce775caf669
loizvla <loizvla@gmail.com>
2022-08-22 13:31:53 +0300

    about.php

---
+++ php_code_about.php
@@ -0,0 +1,30 @@
+<?php
+include("head.php");
+?>
+<!-- about -->
+<div id="about" class="container spacing">
+	<div class="row">
+		<div class="col-lg order-lg-2">
+				<h3 class="mb-4">Media Health Care</h3>
+				<img class="mb-4 img-fluid rounded d-none d-sm-block" src="img/logo.jpg" alt="Portland" height="60px" width="60px">
+				
+			</div>
+			<div class="col-lg order-lg-1">
+				<h3 class="mb-4">Developer Info</h3>
+				<ul>
+				<li>Name: Λοΐζος</li>
+				<li>Surname: Βλασιάδης</li>
+				<li>A.M: 685</li>
+				<li>Πρόγραμμα Μεταπτυχιακών Σπουδών του Τμήματος Πληροφορικής με Εφαρμογές στην Βιοϊατρική και του Τμήματος Πληροφορικής του Πανεπιστημίου Θεσσαλίας</li>
+				<li>Τίτλος Πρόγραμματος: Πληροφορική και Υπολογιστική Βιοίατρική</li>
+				<li>Μάθημα: Πληροφοριακά Συστήματα Βάσεων Βιολογικών και Ιατρικών Δεδομένων </li>
+				<li>Διδάσκων: Τζουραμάνης Θεόδωρος</li
+				<ul>			
+			</div>
+</div>
+<!-- /about -->
+<?php
+include("footer_signup.php");
+include("final.php");
+
+?>
